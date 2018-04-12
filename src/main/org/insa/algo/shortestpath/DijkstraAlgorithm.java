a9059cdc7e7d54b2a52bdaf92395663dc578a3e7
Jerome007 <je.bravais@gmail.com>
2018-04-12 10:33:58 +0200

    dikjstra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,9 +1,15 @@
 package org.insa.algo.shortestpath;
 
+
+import java.util.*;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.insa.algo.utils.BinaryHeap;
+import org.insa.algo.utils.EmptyPriorityQueueException;
+import org.insa.graph.Arc;
 import org.insa.graph.Graph;
 import org.insa.graph.Node;
 import java.util.HashMap;
@@ -33,28 +39,76 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         
         Node current_node = origin;
         
-        BinaryHeap<Label> tas = new BinaryHeap();
+        BinaryHeap<Label> tas = new BinaryHeap<Label>();
         
-        Label label = new Label(current_node,0,null);
+        Label label = new Label(current_node,0,null,null);
+        hm.put(current_node, label);
         
         tas.insert(label);
         
-        Label label_current;
+        Label label_current = null;
+        Node y = null;
+        Label label_y = null;
         
-        while (nb_marques < size_graphe || path_find == true) {
-        	label_current = tas.findMin();
-        	current_node = label_current.node;
+        float cout_y = 0;
+        
+        while (nb_marques < size_graphe && path_find == false) {
+        	System.out.println(nb_marques);
         	
-        	if (hm.containsKey(current_node) != true) {
-        		
+        	try {
+        		label_current = tas.findMin();
+        	}
+        	catch (EmptyPriorityQueueException e) {
+        		path_find = true;
         	}
         	
+        	current_node = label_current.node;
+        	label_current.mark=true;
         	
+        	if (current_node == destination) {
+    			System.out.println("trouvé sale tepu");
+    			path_find = true;
+        	}
+        	tas.deleteMin();
+        	nb_marques++;
+        	
+        	
+        	for (Arc arc: current_node) {
+        		y = arc.getDestination();
+        		if (hm.containsKey(y) != true) {
+        			hm.put(y,new Label(y,-1,current_node,arc));
+        		}
+        		
+        		label_y = hm.get(y);
+        		
+           		
+        		if (label_y.mark == false) {
+        			cout_y = label_y.cost;
+        			
+        			if (label_y.cost > label_current.cost+arc.getLength() || label_y.cost == -1) {
+        				label_y.cost = label_current.cost+arc.getLength();	
+        			}
+        			if (cout_y != -1) {
+        				tas.remove(label_y); //on update le noeud dans le tas
+        			}
+    				tas.insert(label_y);
+        		}
+        	}
         	
         }
         
-        
-        return solution;
+        ArrayList<Arc> listeSol = new ArrayList<Arc>();
+		
+		
+		while(label_current.node != origin) {    				
+			listeSol.add(label_current.father_arc);
+			label_current = hm.get(label_current.father);
+		}
+		
+		Collections.reverse(listeSol);
+		
+		return solution;
+		
     }
 
 }
--- src/main/org/insa/algo/shortestpath/Label.java
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -1,21 +1,25 @@
 package org.insa.algo.shortestpath;
 
+import org.insa.graph.Arc;
 import org.insa.graph.Node;
 
 public class Label implements Comparable<Label>{
 	
 	public Node node;
-	public int cost;
+	public float cost;
 	public Node father;
 	public Boolean mark = false;
+	public Arc father_arc;
 	
-	public Label(Node node, int cost, Node father) {
+	public Label(Node node, float cost, Node father, Arc arc_fath) {
 		this.node = node;
 		this.cost = cost;
 		this.father = father;
+		this.father_arc = arc_fath;
 	}
 	
-		public int compareTo(Object o) {
+
+		public int compareTo(Label o) {
 			Label objet = (Label) o;
 			if (this.cost == objet.cost) return 0;
 			if (this.cost < objet.cost) return -1;
